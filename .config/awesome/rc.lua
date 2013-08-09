e26599c2263d3c17ba3707f005e028c8067b00d0
Sebastian Wild <sebastia.wild@gmail.com>
2013-08-09 12:36:34 +0000

    Fixed awesome on laptop

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -112,13 +112,13 @@ kbdcfg = {}
 kbdcfg.cmd = "setxkbmap"
 kbdcfg.layout = { "us", "de" }
 kbdcfg.current = 1
-kbdcfg.widget = widget({ type = "textbox", align = "right" })
-kbdcfg.widget.text = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+kbdcfg.widget = wibox.widget.textbox()
+kbdcfg.widget:set_text(" " .. kbdcfg.layout[kbdcfg.current] .. " ")
 kbdcfg.switch = function ()
     kbdcfg.current = kbdcfg.current % #(kbdcfg.layout) + 1
-    local t = " " .. kbdcfg.layout[kbdcfg.current] .. " "
-    kbdcfg.widget.text = t
-    os.execute( kbdcfg.cmd .. t )
+    local t = kbdcfg.layout[kbdcfg.current]
+    kbdcfg.widget:set_text(" " .. t .. " ")
+    os.execute(kbdcfg.cmd .. " " .. t .. " ")
 end
 
 kbdcfg.widget:buttons(awful.util.table.join(
