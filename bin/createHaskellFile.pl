324b89ac03462acf91dc6a97532ad2e1b96db865
Sebastian Wild <sebastia.wild@gmail.com>
2014-03-10 21:19:40 +0100

    Added GPL3 to creating a haskell file

--- bin/createHaskellFile.pl
+++ bin/createHaskellFile.pl
@@ -22,7 +22,22 @@ if( defined $ARGV[1] && $ARGV[1] eq "AGPL" ) {
                 'This program is free software: you can redistribute it and/or modify',
                 'it under the terms of the GNU Affero General Public License as published by',
                 'the Free Software Foundation, either version 3 of the License, or',
-                '(at your option) any later version.',
+                'any later version.',
+                '',
+                'This program is distributed in the hope that it will be useful,',
+                'but WITHOUT ANY WARRANTY; without even the implied warranty of',
+                'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the',
+                'GNU Affero General Public License for more details.',
+                '',
+                'You should have received a copy of the GNU Affero General Public License',
+                'along with this program.  If not, see <http://www.gnu.org/licenses/>.');
+}
+elsif( defined $ARGV[1] && $ARGV[1] eq "GPL3" ) {
+    @license = ('',
+                'This program is free software: you can redistribute it and/or modify',
+                'it under the terms of the GNU General Public License as published by',
+                'the Free Software Foundation, either version 3 of the License, or',
+                'any later version.',
                 '',
                 'This program is distributed in the hope that it will be useful,',
                 'but WITHOUT ANY WARRANTY; without even the implied warranty of',
